df9c76b15908138cb6d6c8c4ec8ee086a242fd40
PedroMoreira-a22002701 <pedromrm2001@gmail.com>
2022-06-04 16:01:27 +0100

    Quiz Feito

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -6,8 +6,3 @@ class PostForm(ModelForm):
     class Meta:
         model = post
         fields = '__all__'
-class PontuacaoForm(ModelForm):
-    class Meta:
-        model = PontuacaoQuiz
-        fields = '__all__'
-           
--- portfolio/views.py
+++ portfolio/views.py
@@ -23,6 +23,7 @@ def projectos_page_view(request):
 
 def licenciatura_page_view(request):
     return render(request, 'portfolio/licenciatura.html')
+    
 def post_page_view(request):
     form = PostForm(request.POST or None)
     if form.is_valid():
@@ -32,30 +33,27 @@ def post_page_view(request):
 
     return render(request, 'portfolio/post.html', context)
 
-def quiz_page_view(request):
-    form = quizz(request)
-    quizz(request)
-    context = {'form': form}
 
-    return render(request, 'portfolio/quiz.html', context)
 
 def quizz(request):
     if request.method == 'POST':
-        n = request.POST['nome']
+        n = request.POST['name']
         p = pontuacao_quiz(request)
-        r = PontuacaoQuiz(nome=n, pontuacao=p)
+        r = PontuacaoQuiz(nome=n, pontos=p)
         r.save()
-        return r
+        return render(request, 'portfolio/quiz.html')
+    return render(request, 'portfolio/quiz.html')    
+
 def pontuacao_quiz(request):
     count = 0
-    if(request.POST('0') == 'HyperText Markup Language'):
+    if(request.POST['0'] == 'HyperText Markup Language'):
         count = count + 1
-    if(request.POST('1') == 'HyperText Markup Language'):
+    if(request.POST['1'] == 'Cascading Style Sheet'):
         count = count + 1
-    if(request.POST('2') == 'box-shadow: 10px 10px 5px grey;'):
+    if(request.POST['2'] == 'box-shadow: 10px 10px 5px grey;'):
         count = count + 1    
-    if(request.POST('3') == 'text-wrap: break-word;'):
+    if(request.POST['3'] == 'text-wrap: break-word;'):
         count = count + 1   
-    if(request.POST('4') == 'border-radius: 30px;'):
+    if(request.POST['4'] == 'border-radius: 30px;'):
         count = count + 1             
     return count
