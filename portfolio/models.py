2fc52d4d6df83f3c9cf1caa319ead02ea598f4c0
PedroMoreira-a22002701 <pedromrm2001@gmail.com>
2022-06-07 13:51:37 +0100

    Post

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,7 +5,7 @@ class post(models.Model):
     autor = models.CharField(max_length=50)
     titulo = models.CharField(max_length=20)
     descricao = models.CharField(max_length=200)
-    data = models.DateField()
+    data = models.DateField(auto_now_add=True)
 
 class PontuacaoQuiz(models.Model):
     nome = models.CharField(max_length=50)
